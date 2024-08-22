58a16bc2a03e70c481895d249a9da2f2c0450b7b
InryeolChoi <dlsfuf0316@gmail.com>
2024-08-23 00:06:12 +0900

    fix : remove personal info

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,8 +22,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'dlsfuf0316@gmail.com'
-EMAIL_HOST_PASSWORD = 'kukj wxpb bizi bgbx'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 # Application definition
 
