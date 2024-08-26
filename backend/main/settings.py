387ad3e7158fe67f57d16e5fb9f870bc90d77429
InryeolChoi <dlsfuf0316@gmail.com>
2024-08-26 21:38:01 +0900

    update CORS

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt.token_blacklist',
     # Apps
     'login',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,7 +51,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+# CORS 관련 설정
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [ os.getenv('DJANGO_CORS_ALLOWED_ORIGINS') ]
+CORS_ALLOW_CREDENTIALS = True
+
 
 ROOT_URLCONF = 'main.urls'
 
